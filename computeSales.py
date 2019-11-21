66e2149aa29ef2859c569c5f4c72af9628c9f383
mpantourakis <pantourakis.michail@gmail.com>
2019-11-22 01:58:08 +0200

    Added input messages and Afm validation

--- computeSales.py
+++ computeSales.py
@@ -1,30 +1,55 @@
 from os import system, name
 
+# Constants for messages
+MENU = "Give your preference: (1: read new input file, 2: print statistics for a specific product, 3: print statistics for a specific AFM, 4: exit the program)"
+INVALID_OPTION = "invalidOption"
+ASK_AFM = "Give the AFM: "
+ASK_PRODUCT = "Give the product name: "
+ASK_INPUT = "Give name/path to input file: "
+
+def is_valid_afm(afm):
+    try:
+        int(afm)
+        return len(afm) == 9 and "-" not in afm
+    except ValueError:
+        return False
+
 # function adopted from https://www.geeksforgeeks.org/clear-screen-python/
 def clear_console():
     # for windows 
     if name == 'nt': 
-        _ = system('cls') 
+        _ = system('cls')
     # for mac and linux(here, os.name is 'posix') 
     else: 
         _ = system('clear')
 
-# main function
-def run_app():
-    while True:
-        menu = "Give your preference: (1: read new input file, 2: print statistics for a specific product, 3: print statistics for a specific AFM, 4: exit the program)"
-        choice = input(menu)
-        switcher = MenuSwitcher()
-        switcher.call_chosen_method(choice)
-
-# Switcher class adopted from https://jaxenter.com/implement-switch-case-statement-python-138315.html
 class MenuSwitcher(object):
     def call_chosen_method(self, choice):
         chosen_option = 'option_' + str(choice)
-        option = getattr(self, chosen_option, lambda: "0")
+        option = getattr(self, chosen_option, lambda: None)
         return option()
+
+    def option_1(self):
+        input_file = input(ASK_INPUT)
+
+    def option_2(self):
+        product_name = input(ASK_PRODUCT)
+
+    def option_3(self):
+        afm = input(ASK_AFM)
+        if is_valid_afm(afm):
+            return "Placeholder for valid afm"
 
     def option_4(self):
         exit()
+
+# main function
+def run_app():
+    while True:
+        choice = input(MENU)
+        switcher = MenuSwitcher()
+        result = switcher.call_chosen_method(choice)
+        if result != None:
+            print(result)
 
 run_app()
