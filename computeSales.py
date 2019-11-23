ddd4558af2f4d27b783faf37878bea6ffa68641c
mpantourakis <pantourakis.michail@gmail.com>
2019-11-23 22:24:47 +0200

    Implemented receipt models and stats handler for menu options 2 and 3

--- computeSales.py
+++ computeSales.py
@@ -7,10 +7,76 @@ ASK_AFM = "Give the AFM: "
 ASK_PRODUCT = "Give the product name: "
 ASK_INPUT = "Give name/path to input file: "
 
+class StatsHandler(object):
+    def __init__(self):
+        # data structures for total sales per product/AFM
+        self.product_per_afm_sales = {}
+        self.afm_per_product_sales = {}
+    
+
+    # methods for updating data structures
+    def update_stats(self, receipt):
+        self.update_product_per_afm(receipt)
+        self.update_afm_per_product(receipt)
+
+    def update_product_per_afm(self, receipt):
+        for entry in receipt.entries:
+            if entry.product in self.product_per_afm_sales:
+                afm_sales = self.product_per_afm_sales.get(entry.product)
+                if receipt.afm in afm_sales:
+                    afm_sales[receipt.afm] += entry.total_price
+                else:
+                    afm_sales[receipt.afm] = entry.total_price
+            else:
+                    afm_sales = {receipt.afm : entry.total_price}
+            self.product_per_afm_sales[entry.product] = afm_sales
+        
+    def update_afm_per_product(self, receipt):
+        for entry in receipt.entries:
+            if receipt.afm in self.afm_per_product_sales:
+                product_sales = self.afm_per_product_sales.get(receipt.afm)
+                if entry.product in product_sales:
+                    product_sales[entry.product] += entry.total_price
+                else:
+                    product_sales[entry.product] = entry.total_price
+            else:
+                    product_sales = {entry.product : entry.total_price}
+            self.afm_per_product_sales[receipt.afm] = product_sales
+
+    def afm_to_string(self, product):
+        afm_sales = self.product_per_afm_sales.get(product)
+        if afm_sales:
+            return "\n".join('{} {}'.format(afm, total_price) for afm, total_price in sorted(afm_sales.items()))
+
+    def product_to_string(self, afm):
+        product_sales = self.afm_per_product_sales.get(afm)
+        if product_sales:
+            return "\n".join('{} {}'.format(product, total_price) for product, total_price in sorted(product_sales.items()))
+
+# model classes for parsing input files
+class Receipt(object):
+    def __init__(self, afm):
+        self.total_price = 0.00
+        self.entries = []
+        self.afm = afm
+        
+    def add_entry(self, receipt_entry):
+        self.entries.insert(receipt_entry)
+        self.total_price += receipt_entry.total_price
+
+    def has_correct_total(self, total_price):
+        return self.total_price == total_price
+class ReceiptEntry(object):
+    def __init__(self, product, amount, unit_price, total_price):
+        self.product = product
+        self.amount = amount
+        self.unit_price = unit_price
+        self.total_price = total_price
+
 def is_valid_afm(afm):
     try:
         int(afm)
-        return len(afm) == 9 and "-" not in afm
+        return len(afm) == 10 and "-" not in afm
     except ValueError:
         return False
 
@@ -23,7 +89,10 @@ def clear_console():
     else: 
         _ = system('clear')
 
-class MenuSwitcher(object):
+class MenuHandler(object):
+    def __init__(self, stats_handler):
+        self.stats_handler = stats_handler
+
     def call_chosen_method(self, choice):
         chosen_option = 'option_' + str(choice)
         option = getattr(self, chosen_option, lambda: None)
@@ -33,23 +102,25 @@ class MenuSwitcher(object):
         input_file = input(ASK_INPUT)
 
     def option_2(self):
-        product_name = input(ASK_PRODUCT)
+        product = input(ASK_PRODUCT)
+        return self.stats_handler.afm_to_string(product)
 
     def option_3(self):
         afm = input(ASK_AFM)
         if is_valid_afm(afm):
-            return "Placeholder for valid afm"
+            return self.stats_handler.product_to_string(afm)
 
     def option_4(self):
         exit()
 
 # main function
 def run_app():
+    stats_handler = StatsHandler()
     while True:
         choice = input(MENU)
-        switcher = MenuSwitcher()
-        result = switcher.call_chosen_method(choice)
-        if result != None:
+        menu = MenuHandler(stats_handler)
+        result = menu.call_chosen_method(choice)
+        if result:
             print(result)
 
 run_app()
